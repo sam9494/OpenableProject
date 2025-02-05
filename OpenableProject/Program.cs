02a943301b3dbc75bc2f6217c548f0f143bde701
sam_chong <sam_chong@mayohr.com>
2025-02-05 18:50:13 +0800

    init controller

--- OpenableProject/Program.cs
+++ OpenableProject/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,7 +13,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MapControllers();
 app.UseHttpsRedirection();
 
 var summaries = new[]
---
+++ OpenableProject/SearchController.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenableProject;
+[ApiController]
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+public class SearchController : ControllerBase
+{
+    // GET
+    [HttpGet("Index")]
+    public string Index()
+    {
+       return "Hello World";
+
+    }
+}
