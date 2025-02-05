51feb733f60b7052f7fdd7e11145cd37ed9755b4
sam_chong <sam_chong@mayohr.com>
2025-02-05 19:05:38 +0800

    add SearchRequest

--- OpenableProject/SearchController.cs
+++ OpenableProject/SearchController.cs
@@ -7,10 +7,46 @@ namespace OpenableProject;
 public class SearchController : ControllerBase
 {
     // GET
-    [HttpGet("Index")]
-    public string Index()
+    [HttpGet("Get")]
+    public string Get(SearchRequest searchRequest)
     {
        return "Hello World";
 
     }
+}
+
+public class SearchRequest
+{
+    public DateTime DateTime { get; set; }
+    public int PartySize { get; set; }
+    public CountryCode CountyCode { get; set; }
+    public DistrictCode DistrictCode { get; set; } 
+    public decimal UpperPrice { get; set; }
+    public decimal LowerPrice { get; set; } 
+    
+    //用餐選項 
+    public DiningType DiningType { get; set; }
+    
+    //料理種類
+    public MealType MealType { get; set; }
+    
+    // public string SelectedSorting { get;set; } //TODO: 之後處理 
+    // public string Keyword { get; set; } TODO: 之後處理
+    // public int PageNumber { get; set; } TODO: 之後處理
+}
+
+public enum DiningType
+{
+}
+
+public enum MealType
+{
+}
+
+public enum DistrictCode
+{
+}
+
+public enum CountryCode
+{
 }
