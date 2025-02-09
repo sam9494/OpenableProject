0f5643514650b89b5653164b3e50787463d4c8ef
gitName <a@b.c>
2025-02-09 15:14:49 +0800

    add RestaurantController Get action

---
+++ OpenableProject/RestaurantController.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenableProject;
+[ApiController]
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+public class RestaurantController : ControllerBase
+{
+    // GET
+    [HttpGet("Get")]
+    public List<Restaurant> Get()
+    {
+       return new List<Restaurant>()
+       {
+           new Restaurant
+           {
+               Name = "池上便當",
+               ImageUrl = new Uri("http://池上便當url.com") 
+           },
+           new Restaurant
+           {
+               Name = "八方雲集",
+               ImageUrl = new Uri("http://八方雲集url.com")
+           }
+       };
+    }
+}
+
+public class Restaurant
+{
+    public string Name { get; set; }
+    public Uri ImageUrl { get; set; }
+}
