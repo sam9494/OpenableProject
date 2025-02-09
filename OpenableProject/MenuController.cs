7271e313eba7b841cca2bc78c905abaa1b8b0979
gitName <a@b.c>
2025-02-09 15:35:44 +0800

    add Menu Controllerˆ

---
+++ OpenableProject/MenuController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenableProject;
+[ApiController]
+[Route("api/[controller]")]
+public class MenuController : ControllerBase
+{
+    // GET
+    [HttpGet("Get")]
+    public List<Menu> Get(int restaurantId)
+    {
+        return new List<Menu>()
+        {
+            new Menu()
+            {
+                Id = 100,
+                Name = "雞腿便當",
+                Price = 70m
+            },
+            new Menu()
+            {
+                Id = 101,
+                Name = "招牌便當",
+                Price = 60m
+            }
+        };
+    }
+}
+
+public class Menu
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
