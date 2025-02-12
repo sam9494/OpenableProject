79830a3439a7065a03bd50831f4979e7ac14d34a
Shon Pan <agito850@gmail.com>
2025-02-13 00:38:55 +0800

    [Feat] 無資料庫版

---
+++ OpenTableProject/Model/Order.cs
@@ -0,0 +1,21 @@
+using OpenTable.Application;
+
+namespace OpenTableProject.Model;
+
+public class Order
+{
+    public Guid DishId { get; set; }
+    public int Amount { get; set; }
+    public decimal TotalPrice { get; set; }
+    public string CustomName { get; set; }
+    
+    //DataModel 
+    // public Guid Id { get; set; }
+    // public Guid RestaurantId { get; set; }
+    // public Guid DishId { get; set; }
+    // public int Amount { get; set; }
+    // public decimal TotalPrice { get; set; }
+    // public string CustomName { get; set; }
+    // public DateTime CreatedAt { get; set; }
+    // public OrderStatusEnum Status { get; set; }
+}
---
+++ OpenTableProject/OrderStatusEnum.cs
@@ -0,0 +1,9 @@
+namespace OpenTable.Application;
+
+public enum OrderStatusEnum
+{
+    Unknown = 0,
+    Unstarted = 1,
+    InProgress = 2,
+    Finished = 3,
+}
