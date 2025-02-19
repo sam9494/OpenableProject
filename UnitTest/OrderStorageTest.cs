bb12b7f3e4997d2acf9aed705a29b8499e082812
Arthur <arthur_liu@mayohr.com>
2025-02-19 09:59:26 +0800

    test: Add simplicity tests - ParallelForEach

--- UnitTest/OrderStorageTest.cs
+++ UnitTest/OrderStorageTest.cs
@@ -8,7 +8,6 @@ public class OrderStorageTest
 {
     private const int NumberOfTasks = 80;
     
-    
     [Fact]
     public async Task Add_ShouldBeThreadSafe_WhenCalledConcurrently()
     {
@@ -31,7 +30,6 @@ public class OrderStorageTest
         Assert.Equal(NumberOfTasks , orderIds.Count);  // 確認無 Id 衝突導致的拋棄，使數量與執行次數一致
     }
     
-    
     [Fact]
     public async Task AddByOperator_NotThreadSafe_WhenCalledConcurrently()
     {
@@ -47,6 +45,39 @@ public class OrderStorageTest
             });
         }
         await Task.WhenAll(tasks);
+        
+        var orders = OrderStorage.GetAll();
+        var orderIds = orders.Select(order => order.Id).ToList();
+        Assert.NotEqual(NumberOfTasks, orderIds.Count);  // 確認 Id 衝突導致的拋棄，使數量與執行次數不一致
+    }
+    
+    [Fact]
+    public void Add_ShouldBeThreadSafe_WhenCalledConcurrently_ParallelForEach()
+    {
+        var tasks = Enumerable.Range(0, NumberOfTasks).ToList();
+
+        Parallel.ForEach(tasks, task =>
+        {
+            var order = new Order { CustomerName = $"Order_{task}" };
+            OrderStorage.Add(order);
+        });
+        
+        var orders = OrderStorage.GetAll();
+        var orderIds = orders.Select(order => order.Id).ToList();
+        Assert.Equal(orderIds.Count, orderIds.Distinct().Count());  // 確保 ID 唯一
+        Assert.Equal(NumberOfTasks , orderIds.Count);  // 確認無 Id 衝突導致的拋棄，使數量與執行次數一致
+    }
+    
+    [Fact]
+    public void AddByOperator_NotThreadSafe_WhenCalledConcurrently_ParallelForEach()
+    {
+        var tasks = Enumerable.Range(0, NumberOfTasks).ToList();
+
+        Parallel.ForEach(tasks, task =>
+        {
+            var order = new Order { CustomerName = $"Order_{task}" };
+            OrderStorage.AddByOperator(order);
+        });
         
         var orders = OrderStorage.GetAll();
         var orderIds = orders.Select(order => order.Id).ToList();
