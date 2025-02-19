c281986f3822b9d85f308b682047ef3fcb988e82
宣蓉 潘 <shon_pan@mayohr.com>
2025-02-19 15:51:55 +0800

    Revert "[Refactor] 把檔案名改回去 (´c_`)"
    This reverts commit 979ddd7a46ddf66dded96d5ddb9f878a0cc3d207.

---
+++ OpenTableProject/Model/Dish.cs
@@ -0,0 +1,17 @@
+namespace OpenTableProject.Model;
+
+public class Dish
+{
+    public Dish(Guid id, string imgUrl, string name, decimal price)
+    {
+        Id = id;
+        ImgUrl = imgUrl;
+        Name = name;
+        Price = price;
+    }
+
+    public Guid Id { get; set; }
+    public string ImgUrl { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
