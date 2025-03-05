ba238788f933bd457fc9c0290fa460c7bffff18a
宣蓉 潘 <shon_pan@mayohr.com>
2025-03-05 17:29:13 +0800

    [Feat]新增Menu

--- OpenableProject/Controllers/MenuController.cs
+++ OpenableProject/Controllers/MenuController.cs
@@ -7,24 +7,42 @@ namespace OpenableProject.Controllers;
 public class MenuController : ControllerBase
 {
     [HttpGet]
-    public List<Meal> GetByRestaurant(int restaurantId)
+    public IEnumerable<Menu> GetByRestaurant(int restaurantId)
     {
-        return
-            new List<Meal>
+        var menus = new List<Menu>()
+        {
+            new Menu()
+            {
+                RestaurantId = 1,
+                Meals = new List<Meal>()
                 {
-                    new Meal { Id = 1, RestaurantId = 1, Name = "雞腿便當" },
-                    new Meal { Id = 2, RestaurantId = 1, Name = "排骨便當" },
-                    new Meal { Id = 3, RestaurantId = 2, Name = "漢堡" },
-                    new Meal { Id = 4, RestaurantId = 2, Name = "炸雞" }
+                    new Meal { Id = 1,  Name = "雞腿便當" },
+                    new Meal { Id = 2,  Name = "排骨便當" },
                 }
-                .Where(m => m.RestaurantId == restaurantId)
-                .ToList();
+            },
+            new Menu()
+            {
+                RestaurantId = 2,
+                Meals = new List<Meal>()
+                {
+                    new Meal { Id = 3,  Name = "漢堡" },
+                    new Meal { Id = 4,  Name = "炸雞" }
+                }
+            }
+        };
+
+        return menus.Where(x => x.RestaurantId == restaurantId);
     }
+}
+
+public class Menu
+{
+    public List<Meal> Meals { get; set; }
+    public int RestaurantId { get; set; }
 }
 
 public class Meal
 {
     public int Id { get; set; }
-    public int RestaurantId { get; set; }
     public string Name { get; set; }
 }
