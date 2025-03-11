a1c63d45a33087b50a447b764badde88af2884df
Shon Pan <agito850@gmail.com>
2025-03-11 23:59:33 +0800

    feat: add Swagger Bearer Token setting

--- OpenableProject/Program.cs
+++ OpenableProject/Program.cs
@@ -1,13 +1,48 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OpenableProject.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo{ Title = "OpenTableProject", Version = "v1" });
+    
+    // 安全定義 "Bearer"
+    // Define OAuth2.0 scheme
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization"
+    });
+    
+    // 安全要求
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 builder.Services.AddControllers();
 
 // 其他服務
@@ -36,7 +71,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "OpenTableProject v1");
+        options.OAuthClientId("swagger");
+        options.OAuthAppName("OpenTableProject");
+        options.OAuthUsePkce();
+    });
 }
 
 app.UseAuthentication();
