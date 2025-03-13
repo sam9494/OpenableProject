1c2c812da73903ea7686a3c33c7391f88232aefa
Arthur-Liu <positive0324@gmail.com>
2025-03-13 23:04:39 +0800

    refactor: Remove duplicate code

--- OpenableProject/ExceptionHandlers/OrderNotFoundExceptionHandler.cs
+++ OpenableProject/ExceptionHandlers/OrderNotFoundExceptionHandler.cs
@@ -4,32 +4,36 @@ using Microsoft.AspNetCore.Mvc;
 using OpenableProject.Exceptions;
 
 namespace OpenableProject.ExceptionHandlers;
-public class OrderNotFoundExceptionHandler : IExceptionHandler
-{
-    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
-    {
-        if (exception is not OrderNotFoundException)
-        {
-            return false;
-        }
-    
-        httpContext.Response.StatusCode = OrderNotFoundException.StatusCode;
-        
-        var problemDetails = new ProblemDetails
-        {
-            Status = OrderNotFoundException.StatusCode,
-            Title = OrderNotFoundException.Title,
-            Detail = exception.Message,
-            Instance = httpContext.Request.Path,
-            Type = $"https://httpstatuses.com/{OrderNotFoundException.StatusCode}"
-        };
-    
-        Console.WriteLine($"{problemDetails.Title}: {exception.StackTrace}");
 
-        httpContext.Response.ContentType = "application/problem+json";
-        var result = JsonSerializer.Serialize(problemDetails);
-        await httpContext.Response.WriteAsync(result, cancellationToken: cancellationToken);
+public class OrderNotFoundExceptionHandler()
+    : ExceptionHandler<OrderNotFoundException>(OrderNotFoundException.StatusCode, OrderNotFoundException.Title);
     
-        return true;
-    }
-}
+//     : IExceptionHandler
+// {
+//     public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+//     {
+//         if (exception is not OrderNotFoundException)
+//         {
+//             return false;
+//         }
+//     
+//         httpContext.Response.StatusCode = OrderNotFoundException.StatusCode;
+//         
+//         var problemDetails = new ProblemDetails
+//         {
+//             Status = OrderNotFoundException.StatusCode,
+//             Title = OrderNotFoundException.Title,
+//             Detail = exception.Message,
+//             Instance = httpContext.Request.Path,
+//             Type = $"https://httpstatuses.com/{OrderNotFoundException.StatusCode}"
+//         };
+//     
+//         Console.WriteLine($"{problemDetails.Title}: {exception.StackTrace}");
+//
+//         httpContext.Response.ContentType = "application/problem+json";
+//         var result = JsonSerializer.Serialize(problemDetails);
+//         await httpContext.Response.WriteAsync(result, cancellationToken: cancellationToken);
+//     
+//         return true;
+//     }
+// }
